c50231bc32ddd856695f7c50a9dded4a78db3951
217553 <217553@KB37-1-120-C04>
2024-04-04 11:19:51 +0300

    патч 0.1

--- Kursach_Luzin_Patrakov_321/MainWindow.xaml.cs
+++ Kursach_Luzin_Patrakov_321/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Kursach_Luzin_Patrakov_321
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
